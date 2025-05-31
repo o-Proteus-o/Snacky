49e89876d41c36dfa48d22141b763963ecb6ba0f
Proteus <107575501+o-Proteus-o@users.noreply.github.com>
2025-05-31 18:41:01 +0100

    commented the firebase exception "no longer used"

--- lib/Core/Errors/exceptions.dart
+++ lib/Core/Errors/exceptions.dart
@@ -1,18 +1,16 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
 class MyException implements Exception {
   final String message;
   MyException(this.message);
 }
 
-class FireException extends FirebaseAuthException {
-  FireException({
-    required super.code,
-    super.message,
-    super.email,
-    super.credential,
-    super.phoneNumber,
-    StackTrace? stackTrace,
-    super.tenantId,
-  });
-}
+// class FireException extends FirebaseAuthException {
+//   FireException({
+//     required super.code,
+//     super.message,
+//     super.email,
+//     super.credential,
+//     super.phoneNumber,
+//     StackTrace? stackTrace,
+//     super.tenantId,
+//   });
+// }
