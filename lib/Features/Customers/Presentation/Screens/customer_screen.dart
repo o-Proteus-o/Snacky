a17f016af42a6a605e2876b4e4c311db9ad0acd7
Proteus <107575501+o-Proteus-o@users.noreply.github.com>
2025-05-31 18:53:49 +0100

    update the customer app home screen

--- lib/Features/Customers/Presentation/Screens/customer_screen.dart
+++ lib/Features/Customers/Presentation/Screens/customer_screen.dart
@@ -1,19 +1,88 @@
+import 'package:crystal_navigation_bar/crystal_navigation_bar.dart'; // Add this import
 import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
-class CustomerScreen extends StatelessWidget {
+enum _SelectedTab { home, wishlist, search, post, profile }
+
+class CustomerScreen extends StatefulWidget {
   const CustomerScreen({super.key});
+
+  @override
+  State<CustomerScreen> createState() => _CustomerScreenState();
+}
+
+class _CustomerScreenState extends State<CustomerScreen> {
+  _SelectedTab _selectedTab = _SelectedTab.home;
+
+  void _handleIndexChanged(int index) {
+    setState(() {
+      _selectedTab = _SelectedTab.values[index];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      body: const IndexedStack(index: 0, children: []),
-      // bottomNavigationBar: Padding(
-      //   padding: const EdgeInsets.only(bottom: 10),
-      //   child: CrystalNavigationBar(
-      //     items: [],
-      //   ),
-      // ),
+      body: IndexedStack(
+        index: _SelectedTab.values.indexOf(_selectedTab),
+        children: const [
+          Center(child: Text('Home')),
+          Center(child: Text('wishlist')),
+          Center(child: Text('post')),
+          Center(child: Text('Search')),
+          Center(child: Text('Profile')),
+        ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: CrystalNavigationBar(
+          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+          unselectedItemColor: Colors.white70,
+          backgroundColor: Colors.black.withOpacity(0.5),
+          borderWidth: 2,
+          outlineBorderColor: Colors.white.withOpacity(0.1),
+          onTap: _handleIndexChanged,
+          items: [
+            CrystalNavigationBarItem(
+              icon: LucideIcons.house,
+              unselectedIcon: LucideIcons.house,
+              selectedColor: Colors.white,
+              badge: Badge(
+                label: Text("9+", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+
+            /// Favourite
+            CrystalNavigationBarItem(
+              icon: LucideIcons.heart,
+              unselectedIcon: LucideIcons.heart,
+              selectedColor: Colors.red,
+            ),
+
+            /// Add
+            CrystalNavigationBarItem(
+              icon: LucideIcons.plus,
+              unselectedIcon: LucideIcons.plus,
+              selectedColor: Colors.white,
+            ),
+
+            /// Search
+            CrystalNavigationBarItem(
+              icon: LucideIcons.search,
+              unselectedIcon: LucideIcons.search,
+              selectedColor: Colors.white,
+            ),
+
+            /// Profile
+            CrystalNavigationBarItem(
+              icon: LucideIcons.user,
+              unselectedIcon: LucideIcons.user,
+              selectedColor: Colors.white,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
